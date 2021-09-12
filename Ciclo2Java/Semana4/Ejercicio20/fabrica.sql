460ad65d951bc58f8803a386dfc33cca9aed0211
David Serna Gutiérrez <59783828+dsernag@users.noreply.github.com>
2021-09-11 23:12:12 -0500

    Update fabrica.sql

--- Ciclo2Java/Semana4/Ejercicio20/fabrica.sql
+++ Ciclo2Java/Semana4/Ejercicio20/fabrica.sql
@@ -1,1 +1,5 @@
-CREATE TABLE `fabrica`.`fabrica` ( `numero_fabrica` INT(20) NOT NULL AUTO_INCREMENT , `cantidad_empleados` INT(20) NOT NULL , `pais` TEXT NOT NULL , `gerente` TEXT NOT NULL , PRIMARY KEY (`numero_fabrica`)) ENGINE = InnoDB; 
+CREATE TABLE `fabrica`.`fabrica` ( `numero_fabrica` INT(20) NOT NULL AUTO_INCREMENT , 
+                                  `cantidad_empleados` INT(20) NOT NULL , 
+                                  `pais` TEXT NOT NULL , 
+                                  `gerente` TEXT NOT NULL ,
+                                  PRIMARY KEY (`numero_fabrica`)) ENGINE = InnoDB; 
